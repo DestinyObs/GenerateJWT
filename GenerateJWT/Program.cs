89e3b2f3a2eb01762472a1b126476ddd204236aa
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2023-06-09 12:04:02 +0100

    Add project files.

---
+++ GenerateJWT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace JwtSecretKeyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secretKey = GenerateSecretKey();
+            Console.WriteLine(secretKey);
+        }
+
+        private static string GenerateSecretKey()
+        {
+            var randomBytes = new byte[32];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(randomBytes);
+            }
+            return Convert.ToBase64String(randomBytes);
+        }
+    }
+}
